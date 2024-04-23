2468baa601b50cb5df9d07ed3a24052d933a4d2a
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-04-23 08:54:46 +0200

    Added stuff back to Room

--- src/main/java/com/example/backend/model/Room.java
+++ src/main/java/com/example/backend/model/Room.java
@@ -1,4 +1,25 @@
 package com.example.backend.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int size;
+
+    public Room(int size){
+        this.size = size;
+    }
 }
