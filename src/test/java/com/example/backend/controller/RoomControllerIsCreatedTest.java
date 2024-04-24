582cc38a421b55ea5abf7245ab67ef767f419a50
Martin Harrysson <MartinHarrysson1990@hotmail.com>
2024-04-24 14:06:16 +0200

    Created tests to check if customer,room and booking controllers are created

---
+++ src/test/java/com/example/backend/controller/RoomControllerIsCreatedTest.java
@@ -0,0 +1,18 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class RoomControllerIsCreatedTest {
+    @Autowired
+    private RoomController roomController;
+
+    @Test
+    public void contextLoad(){
+        assertThat(roomController).isNotNull();
+    }
+}
