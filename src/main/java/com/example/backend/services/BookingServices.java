4d7fb568d057af36bb7a78de59cef0947d872ee4
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-04-24 10:49:12 +0200

    Added temporary functionality to BookingServices

--- src/main/java/com/example/backend/services/BookingServices.java
+++ src/main/java/com/example/backend/services/BookingServices.java
@@ -1,7 +1,14 @@
 package com.example.backend.services;
 
+import com.example.backend.Dto.DetailedBookingDto;
+import com.example.backend.Dto.MiniBookingDto;
+import com.example.backend.Dto.MiniCustomerDto;
+import com.example.backend.Dto.MiniRoomDto;
+import com.example.backend.model.Booking;
 import com.example.backend.repos.BookingRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingServices {
@@ -9,5 +16,25 @@ public class BookingServices {
 
     public BookingServices(BookingRepo br){
         this.br = br;
+    }
+
+    public DetailedBookingDto BookingToDetailedBookingDto(Booking b){
+        return DetailedBookingDto.builder().id(b.getId()).beds(b.getExtraBeds())
+                .startDate(b.getStartDate()).endDate(b.getEndDate())
+                .miniRoom(new MiniRoomDto())
+                .miniCustomer(new MiniCustomerDto());
+        id, beds, start date, end date, MiniRoom, MiniCustomer
+    }
+
+    public MiniBookingDto BookingToMiniBookingDto(Booking b){
+        return MiniBookingDto.builder().id(b.getId()).startDate(b.getStartDate())
+                .endDate(b.getEndDate())
+                .miniRoom(new MiniRoomDto())
+                .miniCustomer(new MiniCustomerDto());
+        id, start date, end date, MiniRoom, MiniCustomer
+    }
+
+    public List<DetailedBookingDto> getAllDetailedBookings(){
+        br.findAll().stream().map(b -> BookingToDetailedBookingDto(b)).toList();
     }
 }
