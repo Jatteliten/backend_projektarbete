8d0fc48888d9c589118deeb7737fdd951fb66d7a
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-04-25 12:51:50 +0200

    Added controllers for thymeleaf views

---
+++ src/main/java/com/example/backend/controller/BookingView/BookingUpdateViewController.java
@@ -0,0 +1,9 @@
+package com.example.backend.controller.BookingView;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Booking")
+public class BookingUpdateViewController {
+}
---
+++ src/main/java/com/example/backend/controller/CustomerView/CustomerUpdateViewController.java
@@ -0,0 +1,9 @@
+package com.example.backend.controller.CustomerView;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Customer")
+public class CustomerUpdateViewController {
+}
