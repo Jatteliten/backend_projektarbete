abacaf9d21f7af33fc18a04ebaadc50e15b0033c
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-05-01 09:20:01 +0200

    Removed test

--- src/test/java/com/example/backend/controller/CustomerControllerMockTests.java
+++ src/test/java/com/example/backend/controller/CustomerControllerMockTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,9 +15,6 @@ import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -45,14 +41,7 @@ public class CustomerControllerMockTests {
         when(mockRepo.findAll()).thenReturn(Arrays.asList(c1, c2, c3));
     }
 
-
     @Test
     public void contextLoads() {assertThat(cc).isNotNull();}
 
-    @Test
-    void getCustomerById() throws Exception {
-        this.mvc.perform(get("/customers/getCustomer/1"))
-                .andExpect(status().isOk())
-                .andExpect(content().json("{\"id\":1}"));
-    }
 }
