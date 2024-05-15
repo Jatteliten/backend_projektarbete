da64ebac6326c40e6eae49241e88fbcf6ac3a49b
Lise Martinsen <lise.martinsen@yh.nackademin.se>
2024-05-15 15:49:03 +0200

    basfunkionalitet för att visa händelser för ett rum

--- src/main/java/com/example/backend/controller/RoomView/RoomViewController.java
+++ src/main/java/com/example/backend/controller/RoomView/RoomViewController.java
@@ -1,21 +1,29 @@
 package com.example.backend.controller.RoomView;
 
 
+import com.example.backend.model.modelUti.CleaningEvent;
+import com.example.backend.model.modelUti.DoorEvent;
+import com.example.backend.services.RoomEventServices;
 import com.example.backend.services.RoomServices;
+import com.example.backend.services.impl.RoomEventServicesImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/Room")
 public class RoomViewController {
 
     RoomServices roomServices;
+    RoomEventServices roomEventServices;
 
-    public RoomViewController(RoomServices roomServices) {
+    public RoomViewController(RoomServices roomServices, RoomEventServices roomEventServices) {
         this.roomServices = roomServices;
+        this.roomEventServices = roomEventServices;
     }
 
     @GetMapping("/all")
@@ -27,7 +35,17 @@ public class RoomViewController {
     @RequestMapping("/backlog/{id}")
     public String getBacklog(Model model, @PathVariable Long id) {
         model.addAttribute("RoomdId", id);
-        //Filtrera ut alla door och cleaning events med detta id
+        System.out.println("---------------");
+       // System.out.println(roomEventServices.getEventsByRoomId(id));
+        System.out.println("---------------");
+        model.addAttribute("allEvents", roomEventServices.getEventsByRoomId(id));
+
+        List<CleaningEvent> cleaningEvents = roomEventServices.getCleaningEventsByRoomId(id);
+        List<DoorEvent> doorEvents = roomEventServices.getDoorEventsByRoomId(id);
+        model.addAttribute("cleaningEvents", cleaningEvents);
+        model.addAttribute("doorEvents", doorEvents);
+
         return "Room/backlog.html";
     }
+
 }
