38ac789e097a0e0f4d17995f8df0976b5df64930
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-05-15 15:26:58 +0200

    Added console app for bean initialisation

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -33,84 +33,11 @@ public class BackendApplication {
             SpringApplication application = new SpringApplication(FetchEventQueue.class);
             application.setWebApplicationType(WebApplicationType.NONE);
             application.run(args);
+        } else if(Objects.equals(args[0], "bean")){
+            SpringApplication application = new SpringApplication(BeanInitialisation.class);
+            application.setWebApplicationType(WebApplicationType.NONE);
+            application.run(args);
         }
     }
-
-
-    @Bean
-    public CommandLineRunner commandLineRunner(RoomRepo roomRepo, BookingRepo br, CustomerRepo cr) {
-        return (args -> {
-
-            Room r1 = Room.builder().size(1).pricePerNight(100).build();
-            Room r2 = Room.builder().size(1).pricePerNight(100).build();
-            Room r3 = Room.builder().size(1).pricePerNight(100).build();
-            Room r4 = Room.builder().size(1).pricePerNight(100).build();
-            Room r5 = Room.builder().size(2).pricePerNight(200).build();
-            Room r6 = Room.builder().size(2).pricePerNight(200).build();
-            Room r7 = Room.builder().size(3).pricePerNight(300).build();
-            Room r8 = Room.builder().size(3).pricePerNight(300).build();
-            Room r9 = Room.builder().size(4).pricePerNight(400).build();
-            Room r10 = Room.builder().size(4).pricePerNight(400).build();
-
-            roomRepo.save(r1);
-            roomRepo.save(r2);
-            roomRepo.save(r3);
-            roomRepo.save(r4);
-            roomRepo.save(r5);
-            roomRepo.save(r6);
-            roomRepo.save(r7);
-            roomRepo.save(r8);
-            roomRepo.save(r9);
-            roomRepo.save(r10);
-
-            Customer c1 = Customer.builder()
-                    .firstName("Daniel")
-                    .lastName("Isaksson")
-                    .email("Daniel@hej.se")
-                    .phoneNumber("0755232222")
-                    .build();
-            Customer c2 = Customer.builder()
-                    .firstName("Lise")
-                    .lastName("Martinsen")
-                    .email("Lise@hej.se")
-                    .phoneNumber("0732511663")
-                    .build();
-            Customer c3 = Customer.builder()
-                    .firstName("Martin")
-                    .lastName("Harrysson")
-                    .email("Martin@hej.se")
-                    .phoneNumber("0725523223")
-                    .build();
-            Customer c4 = Customer.builder()
-                    .firstName("Astrid")
-                    .lastName("Rosen")
-                    .email("Astrid@hej.se")
-                    .phoneNumber("0732511655")
-                    .build();
-
-            cr.save(c1);
-            cr.save(c2);
-            cr.save(c3);
-            cr.save(c4);
-
-            LocalDate currentDay = LocalDate.now();
-
-            Booking b1 = new Booking(currentDay.plusDays(3),
-                    currentDay.plusDays(6), 1, r1, c1);
-
-            Booking b2 = new Booking(currentDay.plusMonths(1).plusDays(3),
-                    currentDay.plusMonths(1).plusDays(5), 1, r1, c1);
-
-            Booking b3 = new Booking(currentDay.plusDays(1),
-                    currentDay.plusDays(6), 1, r4, c2);
-
-            br.save(b1);
-            br.save(b2);
-            br.save(b3);
-
-        });
-    }
-
-
 
 }
