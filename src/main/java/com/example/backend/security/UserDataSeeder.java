84e20d516cdbf92b14a670685f9cc4d19dec7658
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-05-21 17:00:06 +0200

    removed unused imports

--- src/main/java/com/example/backend/security/UserDataSeeder.java
+++ src/main/java/com/example/backend/security/UserDataSeeder.java
@@ -1,7 +1,5 @@
 package com.example.backend.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
