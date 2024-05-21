a5b39b455765b6ccdeb3e006cf4395f20ffccff6
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-05-21 16:13:50 +0200

    Added role and user

---
+++ src/main/java/com/example/backend/security/User.java
@@ -0,0 +1,40 @@
+package com.example.backend.security;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "User")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    @Column(name="id")
+    private UUID id;
+
+
+
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
+}
