26b19bc842c251c9c04793b2c359e046542eaf4e
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-05-23 09:03:15 +0200

    Changed to glassfish 4.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("org.hibernate.validator:hibernate-validator:6.2.0.Final")
     implementation ("jakarta.validation:jakarta.validation-api:3.0.0")
     implementation ("org.hibernate.validator:hibernate-validator:7.0.1.Final")
-    implementation ("org.glassfish:jakarta.el:3.0.3")
+    implementation ("org.glassfish:jakarta.el:4.0.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.13.0")
     implementation("com.rabbitmq:amqp-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
