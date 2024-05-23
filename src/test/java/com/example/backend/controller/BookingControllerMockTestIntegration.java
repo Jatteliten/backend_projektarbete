690cf9dcf80d76fbc35faeaf97c8fbfb5dbce5e4
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-05-23 10:25:34 +0200

    Changed two test classes to Integration tests

---
+++ src/test/java/com/example/backend/controller/BookingControllerMockTestIntegration.java
@@ -0,0 +1,20 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookingControllerMockTestIntegration {
+    @Autowired
+    private BookingController bc;
+
+    @Test
+    public void contextLoads() {assertThat(bc).isNotNull();}
+
+}
