8b391aaf09bcf545f295a0e66bd11262c3b51854
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-05-23 20:48:58 +0200

    reset password now sends a reset password link

--- src/main/java/com/example/backend/controller/LoginController.java
+++ src/main/java/com/example/backend/controller/LoginController.java
@@ -2,20 +2,31 @@ package com.example.backend.controller;
 
 
 import com.example.backend.EmailSender;
+import com.example.backend.security.PasswordLink;
+import com.example.backend.security.UserRepository;
+import com.example.backend.services.PasswordLinkServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 @Controller
 public class LoginController {
 
     private final EmailSender emailSender;
+    private final UserRepository userRepository;
+    PasswordLinkServices passwordLinkServices;
 
     @Autowired
-    public LoginController(EmailSender emailSender){
+    public LoginController(EmailSender emailSender, UserRepository userRepository, PasswordLinkServices passwordLinkServices){
         this.emailSender = emailSender;
+        this.userRepository = userRepository;
+        this.passwordLinkServices = passwordLinkServices;
     }
 
     @GetMapping("/login")
@@ -27,11 +38,19 @@ public class LoginController {
     public String forgottenPW() {
         return "forgottenPasswordPage";
     }
+
     @PostMapping("/forgottenPW")
-    public String handleForgottenPassword(@RequestParam("email") String email) {
-        String emailText = "tillfällig text - ändra senare";
-        String subject = "Subject";
-        emailSender.sendEmail(email, subject, emailText);
+    public String handleForgottenPassword(@RequestParam("username") String username) {
+        PasswordLink passwordLink = PasswordLink.builder()
+                .timeSent(LocalDate.now())
+                .user(userRepository.getUserByUsername(username))
+                .linkKey(UUID.randomUUID().toString())
+                .build();
+        passwordLinkServices.saveNewPassWordLink(passwordLink);
+        String emailText = "To reset password, please use the following link: \n"
+                + passwordLinkServices.generateCreateNewPasswordLink(passwordLink);
+        String subject = "Reset Password - Asmadali";
+        emailSender.sendEmail(username, subject, emailText);
 
         return "loginPage";
     }
