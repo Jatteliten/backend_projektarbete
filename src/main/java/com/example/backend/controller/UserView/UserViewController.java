8fc18ef4d57c24110dabb13200962a2447b2c78e
Astrid Rosen <astrid.flisan@gmail.com>
2024-05-24 19:54:10 +0200

    metoder och sida för att visa users

--- src/main/java/com/example/backend/controller/UserView/UserViewController.java
+++ src/main/java/com/example/backend/controller/UserView/UserViewController.java
@@ -7,6 +7,8 @@ import com.example.backend.security.UserDetailsServiceImpl;
 import com.example.backend.security.UserRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,21 +46,38 @@ public class UserViewController {
         model.addAttribute("header", "Users");
         model.addAttribute("allUsers", concreteUsers);
 
-        return "manageUsers.html";
+        return "User/manageUsers.html";
 
     }
     @GetMapping("/addUser")
     @PreAuthorize("isAuthenticated()")
     public String addUser() {
-        return "addUser.html";
+        return "User/addUser.html";
     }
 
+    @PostMapping("/addUser")
+    @PreAuthorize("isAuthenticated()")
+    public String addUser(@RequestParam String username, @RequestParam String password,
+                          @RequestParam boolean enabled, Model model){
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("enabled", enabled);
 
+        try {
+            UserDetails test = userDetailsService.loadUserByUsername(username);
+
+            model.addAttribute("header", "User already exists");
+        } catch (UsernameNotFoundException e) {
+            // lägg till user i database
+        }
+
+        return addUser();
+    }
 
     @GetMapping("/editUser")
     @PreAuthorize("isAuthenticated()")
     public String editUser() {
-        return "editUser.html";
+        return "User/editUser.html";
     }
 
 
