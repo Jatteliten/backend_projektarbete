06277cb2a66cc76a01eaebb4c38a5b8dfbe8c3e9
Lise Martinsen <lise.martinsen@yh.nackademin.se>
2024-05-24 22:36:15 +0200

    FIxade bugg i FetchContractCustomersTestIntegration

--- src/test/java/com/example/backend/FetchContractCustomersTestIntegration.java
+++ src/test/java/com/example/backend/FetchContractCustomersTestIntegration.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -16,6 +17,7 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
 class FetchContractCustomersTestIntegration {
     @Autowired
     ContractCustomerRepo contractCustomerRepo;
