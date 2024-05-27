03f43232459a98c574e74fb25e162e5a515205bb
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-05-27 11:50:51 +0200

    added correct user attributes and fixed some thymeleaf issues

--- src/main/java/com/example/backend/controller/UserView/UserViewController.java
+++ src/main/java/com/example/backend/controller/UserView/UserViewController.java
@@ -1,6 +1,8 @@
 package com.example.backend.controller.UserView;
 
 import com.example.backend.security.ConcreteUserDetails;
+import com.example.backend.security.Role;
+import com.example.backend.security.RoleRepository;
 import com.example.backend.security.User;
 import com.example.backend.security.UserDetailsServiceImpl;
 import com.example.backend.security.UserRepository;
@@ -8,21 +10,25 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @RequestMapping("/Users")
 public class UserViewController {
     private UserRepository userRepo;
+    private RoleRepository roleRepository;
     private UserDetailsServiceImpl userDetailsService;
-    public UserViewController(UserDetailsServiceImpl userDetailsService, UserRepository userRepo) {
+    public UserViewController(UserDetailsServiceImpl userDetailsService, UserRepository userRepo, RoleRepository roleRepository) {
         this.userDetailsService = userDetailsService;
         this.userRepo = userRepo;
+        this.roleRepository = roleRepository;
     }
 
     @GetMapping("/viewUsers")
@@ -55,7 +61,8 @@ public class UserViewController {
     @PostMapping("/addUser")
     @PreAuthorize("isAuthenticated()")
     public String addUser(@RequestParam String username, @RequestParam String password,
-                          @RequestParam boolean enabled, Model model){
+                          @RequestParam boolean enabled, @RequestParam boolean admin,
+                          @RequestParam boolean recep,  Model model){
         model.addAttribute("username", username);
         model.addAttribute("password", password);
         model.addAttribute("enabled", enabled);
@@ -65,8 +72,24 @@ public class UserViewController {
 
             model.addAttribute("header", "Users already exists");
         } catch (UsernameNotFoundException e) {
+            ArrayList<Role> roles = new ArrayList<>();
+            if(admin){
+                roles.add(roleRepository.findByName("Admin"));
+            }
+            if(recep){
+                roles.add(roleRepository.findByName("Receptionist"));
+            }
 
-            // lägg till kod för att lägga till user i database
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            String hash = encoder.encode(password);
+
+            User user = User.builder()
+                    .username(username)
+                    .password(hash)
+                    .enabled(enabled)
+                    .roles(roles)
+                    .build();
+            userRepo.save(user);
         }
 
         return "Users/manageUsers.html";
@@ -75,15 +98,12 @@ public class UserViewController {
     @GetMapping("/editUser/{username}")
     @PreAuthorize("isAuthenticated()")
     public String editUser(@PathVariable String username, Model model) {
-        UserDetails user = userDetailsService.loadUserByUsername(username);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("password", user.getPassword());
-        model.addAttribute("enabled", user.isEnabled());
+        getUserAttributes(username, model);
 
         return "Users/editUser.html";
     }
 
-    @PostMapping("editUser")
+    @PostMapping("/editUser")
     @PreAuthorize("isAuthenticated()")
     public String updateUser() {
 
@@ -97,26 +117,31 @@ public class UserViewController {
     @GetMapping("/deleteUser/{username}")
     @PreAuthorize("isAuthenticated()")
     public String deleteUser(@PathVariable String username, Model model) {
-        UserDetails user = userDetailsService.loadUserByUsername(username);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("password", user.getPassword());
-        model.addAttribute("enabled", user.isEnabled());
-
-
-
-        model.addAttribute("role", user.getAuthorities());
+        getUserAttributes(username, model);
 
         return "Users/deleteUser.html";
     }
 
-    @PostMapping("deleteUser")
+    @PostMapping("/deleteUser")
     @PreAuthorize("isAuthenticated()")
     public String deleteUser() {
 
-        //lägg till kod för att ta bort user
-
         return "Users/manageUsers.html";
 
+    }
+
+    private void getUserAttributes(@PathVariable String username, Model model) {
+        User user = userRepo.getUserByUsername(username);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("password", user.getPassword());
+        model.addAttribute("enabled", user.isEnabled());
+        List<Role> roles = user.getRoles();
+        if(roles.contains(roleRepository.findByName("Admin"))){
+            model.addAttribute("adminRole" , true);
+        }
+        if(roles.contains(roleRepository.findByName("Receptionist"))){
+            model.addAttribute("recepRole" , true);
+        }
     }
 
 }
--- src/main/java/com/example/backend/security/User.java
+++ src/main/java/com/example/backend/security/User.java
@@ -35,4 +35,5 @@ public class User {
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
 
+
 }
